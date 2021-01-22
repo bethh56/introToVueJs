5f42a88937dde5c40c28319a3144b2ebb373638f
Beth <bethhnielsen56@gmail.com>
2021-01-21 21:27:48 -0600

    started course

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: "#app",
+  data() {
+    return{
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue= '';
+    }
+  }
+});
